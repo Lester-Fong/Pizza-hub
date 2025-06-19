43190ce7d7553f952d881f80e75b05da21636b76
Lester Fong <lesterfonggi123@gmail.com>
2025-06-19 22:01:05 +0800

    refactor: update user registration and login functions to use custom axios instance for improved error handling

--- quasar/src/boot/axios.js
+++ quasar/src/boot/axios.js
@@ -1,23 +1,33 @@
 import axios from 'axios'
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
-const registerUser = async () => {
-  try {
-    // Use relative path to enable proxy
-    const response = await axios.get('/api/version')
-    return response.data
-  } catch (error) {
-    console.error('Error registering user:', error)
-    throw error
-  }
+const $user_request = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
+
+const registerUser = async (credentials) => {
+  // Use relative path to enable proxy
+  const response = await $user_request.post('/api/register', credentials)
+  return response.data
 }
+
 const loginUser = async (credentials) => {
-  try {
-    const response = await axios.post('/api/login', credentials)
-    return response.data
-  } catch (error) {
-    console.error('Error logging in user:', error)
-    throw error
+  const creds = {
+    email: credentials.email,
+    password: credentials.password,
   }
+
+  const res = await axios.get('/sanctum/csrf-cookie')
+  if (res.status !== 200) {
+    throw new Error('Failed to get CSRF cookie')
+  }
+
+  const response = await $user_request.post('/api/login', creds)
+  return response.data
 }
 
 export { registerUser, loginUser }

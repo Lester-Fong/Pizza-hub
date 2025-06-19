389daf61d43495d37a5ce77d3ff147c8b1675ce9
Lester Fong <lesterfonggi123@gmail.com>
2025-06-19 13:50:06 +0800

    feat: created controller for user auth functions

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterUserRequest;
+use App\Http\Requests\LoginUserRequest;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Log;
+use Config;
 
 class UserController extends Controller
 {
-    //
+    public function register(RegisterUserRequest $request, UserService $user_service)
+    {
+        try {
+            return $user_service->registerUser($request->validated());
+        } catch (\Exception $e) {
+            Log::error('Registration error: ' . $e->getMessage());
+            return response()->json(["error" => true, "message" => "An error occurred"], 500);
+        }
+    }
+
+    public function login(LoginUserRequest $request, UserService $user_service)
+    {
+        $data = $request->validated();
+
+        try {
+            return $user_service->loginUser($data);
+        } catch (\Exception $e) {
+            Log::error('Login error: ' . $e->getMessage());
+            return response()->json(['error' => true, 'message' => 'An error occurred'], 500);
+        }
+    }
+
+    public function logout(Request $request, UserService $user_service)
+    {
+        try {
+            return $user_service->logoutUser($request);
+        } catch (\Exception $e) {
+            Log::error('Logout error: ' . $e->getMessage());
+            return response()->json(['error' => true, 'message' => 'An error occurred'], 500);
+        }
+    }
 }

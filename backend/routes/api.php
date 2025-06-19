5c56836b8c001ea1f58f23d2c4c48eaf63f5fd08
Lester Fong <lesterfonggi123@gmail.com>
2025-06-19 14:00:23 +0800

    refactor: change logout route to use GET method within auth middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,4 +21,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', [UserController::class, 'logout']);
+});

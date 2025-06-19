80e76375f7c4150cdba720e7c40ff612e97c1907
Lester Fong <lesterfonggi123@gmail.com>
2025-06-19 21:59:10 +0800

    feat: create OrderDetails model for managing order details in the database

---
+++ backend/app/Models/OrderDetails.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetails extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_details';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'order_details_id',
+        'order_id',
+        'pizza_id',
+        'quantity',
+    ];
+}

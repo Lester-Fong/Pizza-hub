586fc624a3245e7e4c4921d34d10c58b6ac923b7
Lester Fong <lesterfonggi123@gmail.com>
2025-06-19 22:01:50 +0800

    feat: refactor router setup and enhance authentication handling

--- quasar/src/router/index.js
+++ quasar/src/router/index.js
@@ -12,18 +12,29 @@ import routes from './routes'
  */
 
 export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  const createHistory = process.env.SERVER ? createMemoryHistory : process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  })
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
+  Router.beforeEach((to, from, next) => {
+    // ADMIN
+    if (to.matched.some((m) => m.meta.requiresAuth === true)) {
+      if (localStorage.getItem('access_token') === null) {
+        localStorage.clear()
+        return next({ name: 'LoginPage' })
+      }
+    } else {
+      if (to.matched.some((m) => m.meta.isUser === true)) {
+        if (localStorage.getItem('access_token') != null) {
+          localStorage.clear()
+        }
+      }
+    }
+    next()
   })
 
   return Router
